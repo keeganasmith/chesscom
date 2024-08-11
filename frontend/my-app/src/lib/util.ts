a4065af072bf4cdd6fa59d1014749964f2629b25
Keegan Smith <keeganasmith2003@gmail.com>
2024-08-11 17:11:45 -0500

    only play legal moves on frontend

---
+++ frontend/my-app/src/lib/util.ts
@@ -0,0 +1,32 @@
+/*
+ * Helper functions for some examples
+ */
+
+import {Chess,SQUARES, type Square} from 'chess.js';
+import type { Chessground } from 'svelte-chessground-ui';
+
+
+// Find all legal moves
+export function toDests(chess : Chess) {
+	const dests = new Map();
+	SQUARES.forEach(s => {
+		const ms = chess.moves({square: s, verbose: true});
+		if (ms.length) dests.set(s, ms.map(m => m.to));
+	});
+	return dests;
+}
+
+// Play a move and toggle whose turn it is
+export function playOtherSide(chessground : Chessground,chess : Chess) {
+	return (orig : string,dest : string) => {
+		chess.move({ from: orig, to: dest });
+		const color = chess.turn() == 'w' ? 'white' : 'black';
+		chessground.set({
+			turnColor: color,
+			movable: {
+				color: color,
+				dests: toDests(chess)
+			}
+		});
+	};
+}
--- frontend/my-app/src/routes/+page.svelte
+++ frontend/my-app/src/routes/+page.svelte
@@ -1,2 +1,26 @@
+<script lang="ts">
+    import {Chessground} from 'svelte-chessground-ui';
+    import {Chess} from 'chess.js';
+	import {onMount} from 'svelte';
+	import {toDests,playOtherSide} from '$lib/util.js';
+    const chess = new Chess();
+	let chessground : Chessground;
+    let turnColor = "white"
+    
+	
+	onMount(async () => {
+		chessground.set( {
+			movable: { 
+                free: false, 
+                dests: toDests(chess), 
+                events: {
+                    after: playOtherSide( chessground, chess ) 
+                } 
+            }
+		});
+	});
+    
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Chessground bind:this={chessground} {turnColor}/>
